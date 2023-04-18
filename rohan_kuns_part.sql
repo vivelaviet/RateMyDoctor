45c7ce2af04c908046377d1ae112c11000e7c602
rgovat <rkg0094@tamu.edu>
2023-04-18 01:20:57 -0500

    Created all tables with keys and relationships

--- rohan_kuns_part.sql
+++ rohan_kuns_part.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 18, 2023 at 03:46 AM
+-- Generation Time: Apr 18, 2023 at 08:17 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,8 +32,37 @@ CREATE TABLE `appointment` (
   `Date` date DEFAULT NULL,
   `Time` time DEFAULT NULL,
   `Place` varchar(45) DEFAULT NULL,
-  `DoctorID` int(11) DEFAULT NULL,
-  `CustomerID` int(11) DEFAULT NULL
+  `DoctorID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `CustomerID` int(11) NOT NULL,
+  `FirstName` varchar(45) NOT NULL,
+  `LastName` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Age` int(11) DEFAULT NULL,
+  `Insurance` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `DoctorID` int(11) NOT NULL,
+  `FirstName` varchar(45) NOT NULL,
+  `LastName` varchar(45) NOT NULL,
+  `Specialization` varchar(45) DEFAULT NULL,
+  `Location` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -48,6 +77,19 @@ CREATE TABLE `education` (
   `Level` varchar(45) DEFAULT NULL,
   `Subject` varchar(45) DEFAULT NULL,
   `Institution` varchar(45) DEFAULT NULL,
+  `Date` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `medicalhistory`
+--
+
+CREATE TABLE `medicalhistory` (
+  `MedicalHistoryID` int(11) NOT NULL,
+  `CustomerID` int(11) NOT NULL,
+  `Description` varchar(45) NOT NULL,
   `Date` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -74,10 +116,10 @@ CREATE TABLE `pastemployment` (
 
 CREATE TABLE `ratings` (
   `RatingID` int(11) NOT NULL,
-  `Score` int(11) DEFAULT NULL,
+  `Score` int(11) NOT NULL,
   `Comment` varchar(45) DEFAULT NULL,
   `Date` date DEFAULT NULL,
-  `DoctorID` int(11) DEFAULT NULL
+  `DoctorID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -88,25 +130,49 @@ CREATE TABLE `ratings` (
 -- Indexes for table `appointment`
 --
 ALTER TABLE `appointment`
-  ADD PRIMARY KEY (`AppointmentID`);
+  ADD PRIMARY KEY (`AppointmentID`),
+  ADD KEY `DoctorID` (`DoctorID`),
+  ADD KEY `CustomerID` (`CustomerID`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`DoctorID`);
 
 --
 -- Indexes for table `education`
 --
 ALTER TABLE `education`
-  ADD PRIMARY KEY (`EducationID`);
+  ADD PRIMARY KEY (`EducationID`),
+  ADD KEY `DoctorID` (`DoctorID`);
+
+--
+-- Indexes for table `medicalhistory`
+--
+ALTER TABLE `medicalhistory`
+  ADD PRIMARY KEY (`MedicalHistoryID`),
+  ADD KEY `CustomerID` (`CustomerID`);
 
 --
 -- Indexes for table `pastemployment`
 --
 ALTER TABLE `pastemployment`
-  ADD PRIMARY KEY (`PastEmploymentID`);
+  ADD PRIMARY KEY (`PastEmploymentID`),
+  ADD KEY `DoctorID` (`DoctorID`);
 
 --
 -- Indexes for table `ratings`
 --
 ALTER TABLE `ratings`
-  ADD PRIMARY KEY (`RatingID`);
+  ADD PRIMARY KEY (`RatingID`),
+  ADD KEY `DoctorID` (`DoctorID`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -119,10 +185,28 @@ ALTER TABLE `appointment`
   MODIFY `AppointmentID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `CustomerID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `doctor`
+--
+ALTER TABLE `doctor`
+  MODIFY `DoctorID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `education`
 --
 ALTER TABLE `education`
   MODIFY `EducationID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `medicalhistory`
+--
+ALTER TABLE `medicalhistory`
+  MODIFY `MedicalHistoryID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `pastemployment`
@@ -135,6 +219,41 @@ ALTER TABLE `pastemployment`
 --
 ALTER TABLE `ratings`
   MODIFY `RatingID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `appointment`
+--
+ALTER TABLE `appointment`
+  ADD CONSTRAINT `appointment_ibfk_1` FOREIGN KEY (`DoctorID`) REFERENCES `doctor` (`DoctorID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `appointment_ibfk_2` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `education`
+--
+ALTER TABLE `education`
+  ADD CONSTRAINT `education_ibfk_1` FOREIGN KEY (`DoctorID`) REFERENCES `doctor` (`DoctorID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `medicalhistory`
+--
+ALTER TABLE `medicalhistory`
+  ADD CONSTRAINT `medicalhistory_ibfk_1` FOREIGN KEY (`CustomerID`) REFERENCES `customer` (`CustomerID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `pastemployment`
+--
+ALTER TABLE `pastemployment`
+  ADD CONSTRAINT `pastemployment_ibfk_1` FOREIGN KEY (`DoctorID`) REFERENCES `doctor` (`DoctorID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `ratings`
+--
+ALTER TABLE `ratings`
+  ADD CONSTRAINT `ratings_ibfk_1` FOREIGN KEY (`DoctorID`) REFERENCES `doctor` (`DoctorID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
