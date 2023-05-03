4209226e35dd5cb58ca1d79aa4668a78b557dd5a
vivelaviet <doomixdoomix@yahoo.com>
2023-05-03 17:23:41 -0500

    add user roles

--- Login.php
+++ Login.php
@@ -70,9 +70,42 @@
       </nav>
 </header>
 <body>
+
+    <?php 
+    include_once 'db.php';
+    session_start();
+
+    if(!isset($_SESSION['role'])) {
+        $_SESSION['role'] = 'customer';
+    }
+
+
+    if (isset($_POST['admin'])) {
+        $_SESSION['role'] = 'admin';
+    } else if (isset($_POST['scheduler'])) {
+        $_SESSION['role'] = 'scheduler';
+    } else if (isset($_POST['customer'])) {
+        $_SESSION['role'] = 'customer';
+    }
+
+    echo "You are a ".$_SESSION['role'];
+
+    ?>
+
+    <form action=""  method="post">
+
+        <center>
+        <button type="admin" class="btn btn-danger" name="admin" value="admin">Admin</button>
+        <button type="scheduler" class="btn btn-info" name="scheduler" value="scheduler">Scheduler</button>
+        <button type="customer" class="btn btn-primary" name="customer" value="customer">Customer</button>
+        </center>
+    </form>
+
+
+
+    <!--
     <h1>Login</h1>
 	<form>
-		
         <p>
             <label for="username">Username:</label>
             <input type="text" id="username" name="username">
@@ -83,5 +116,20 @@
             <input type="submit" value="Login">
         </p>
 	</form>
+
+    <h1>Register</h1>
+	<form>
+        <p>
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username">
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+
+            <input type="submit" value="Register">
+        </p>
+	</form>
+-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 </html>
