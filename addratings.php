8ecee8279f5811837d45513e14168874d6f16030
vivelaviet <doomixdoomix@yahoo.com>
2023-05-03 23:21:32 -0500

    refactored headers

--- addratings.php
+++ addratings.php
@@ -10,78 +10,9 @@
     <title>Home</title>
 </head>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Rate my Doctor</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <div class="row">
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/index.php">Home</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/viewdoctors.php">View All Doctors</a>
-                </div>
-                <?php 
-                session_start();
-                if(!isset($_SESSION['role'])) {
-                    $_SESSION['role'] = 'customer';
-                }
-                if ($_SESSION['role'] == 'admin') {
-                    echo '
-                    <div class="col-auto">
-                    <a class="nav-item nav-link" href="/adddoctors.php">Add/Update Doctors</a>
-                    </div>';
-                }
-                ?>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/addcustomers.php">Add/Update Customers</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/viewcustomer.php">View All Customers</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/appointment.php">Add/Update Appointment</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/View_appointment.php">View All Appointment</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/addratings.php">Add/Update Ratings</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/viewratings.php">View All Ratings</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/contact.php">Contact Us</a>
-                </div>
-                <div class="col-auto">
-                    <a class="nav-item nav-link" href="/login.php">Login</a>
-                </div>
-                <div class="col-auto">
-                
-                    <form class="form-inline" style="white-space:nowrap;">
-                        <div class="col-md-auto">
-                            <input class="form-control mr-sm-2" type="search" size="15" name="FnameInput" placeholder="Search First Name" aria-label="Search">
-                        </div>
-                        <div class="col-md-auto">
-                            <input class="form-control mr-sm-2" type="search" size="15" name="LnameInput" placeholder="Search Last Name" aria-label="Search">
-                        </div>
-                        <div class="col-md-auto">
-                            <input class="form-control mr-sm-2" type="search" size="8" placeholder="Location" aria-label="Search">
-                        </div>
-                        <div class="col-auto">
-                            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                        </div>
-                    </form>
-
-                </div>
-                
-          </div>
-        </div>
-      </nav>
+<?php 
+    include_once 'header.php';
+    ?>
 </header>
 <body>
     <h1>Add Ratings</h1>
